078e8f6b6545c12af5753deb21570247508391ce
Abneia <dssxlvrchat59@tutamail.com>
2024-09-29 15:51:32 -0400

    Add files via upload

---
+++ README.md
@@ -0,0 +1,24 @@
+# XOIC
+Fast DoS &amp; DDoS tool inspired by HOIC
+
+# How to install
+ 
+apt update && apt upgrade
+
+pkg install  python
+
+pkg install git
+
+git clone 
+https://github.com/StormRLS/XOIC
+
+cd XOIC
+​
+# Main Tool:
+python XOIC.py
+
+# Hours 2 Seconds
+python h2s.py
+
+# Host 2 Up
+python host2ip.py
---
+++ host2ip.py
@@ -0,0 +1,23 @@
+import socket
+import colorama
+from colorama import Fore, Style
+print(Fore.MAGENTA + Style.BRIGHT)
+print("\n" * 100)
+print("""
+
+     8888   8888 8888888 88 8888888
+      8888 8888  88   88 88 88
+       8888888   88   88 88 88
+      8888 8888  88   88 88 88
+     8888   8888 8888888 88 8888888
+               alpha v0.1
+
+""")
+
+print(Fore.BLUE + Style.BRIGHT)
+ip = " "
+
+ip = input("Enter Host: " + Fore.MAGENTA)
+
+x = socket.gethostbyname(ip)
+print("ip: " + Fore.BLUE, x)
